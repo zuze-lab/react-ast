7622bace6f9c98a2bad55eaac6db085fc47b3cc8
akmjenkins <akmjenkins@gmail.com>
2020-10-13 22:17:58 -0300

    Updates

---
+++ precache-manifest.5d1c6f0f7f307d9c37cdd093ec859ea8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa333037a276dab945aed9141eac41ec",
+    "url": "/react-ast/index.html"
+  },
+  {
+    "revision": "388425dd7ad11dfc4ac9",
+    "url": "/react-ast/static/js/0.6f128606.chunk.js"
+  },
+  {
+    "revision": "abc254023ed071504566",
+    "url": "/react-ast/static/js/1.2ce665e3.chunk.js"
+  },
+  {
+    "revision": "f8811a73a9ce0353b8fa",
+    "url": "/react-ast/static/js/2.d852517d.chunk.js"
+  },
+  {
+    "revision": "7be828228d0c0a956cc8",
+    "url": "/react-ast/static/js/3.e40e2763.chunk.js"
+  },
+  {
+    "revision": "a90e3cb8a4aaf239db1a",
+    "url": "/react-ast/static/js/6.75ccb04d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-ast/static/js/6.75ccb04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8d36b06a6b93306f88",
+    "url": "/react-ast/static/js/main.f2ec68c9.chunk.js"
+  },
+  {
+    "revision": "19ea3cfdf0fe13098623",
+    "url": "/react-ast/static/js/runtime-main.423ada5d.js"
+  }
+]);
